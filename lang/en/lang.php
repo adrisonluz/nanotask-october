0b57e584e23917b117346fad48f725665cf90d87
Adrison Luz <adrison701@gmail.com>
2017-08-30 09:30:57 -0300

    Commit inicial

---
+++ lang/en/lang.php
@@ -0,0 +1,6 @@
+<?php return [
+    'plugin' => [
+        'name' => 'Nano Task',
+        'description' => 'Nano Task é um gerenciador tarefas simples e prático onde você pode criar e gerenciar tarefas entre você e sua equipe de maneira rápida e eficaz.'
+    ]
+];
